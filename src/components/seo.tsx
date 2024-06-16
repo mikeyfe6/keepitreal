68b9b85d63a779be0eca41d37c187b827c9809f4
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-06-16 20:39:02 +0200

    preparing the site for search console (part3)
    added og meta's for socials

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -41,19 +41,31 @@ export const Seo: React.FC<SEOProps> = ({
 			<title>{seo.title}</title>
 			<meta name='description' content={seo.description} />
 			<meta name='image' content={seo.image} />
-
 			{keywords && <meta name='keywords' content={keywords} />}
 
-			{/* SOCIAL MEDIA METATAGS */}
+			{/* OPEN GRAPH META TAGS */}
+			{seo.title && <meta property='og:title' content={seo.title} />}
+			{seo.description && (
+				<meta property='og:description' content={seo.description} />
+			)}
+			{seo.image && <meta property='og:image' content={seo.image} />}
+			<meta property='og:site_name' content={siteTitle} />
+			{seo.url && <meta property='og:url' content={seo.url} />}
+			<meta property='og:locale' content='nl_NL' />
+			<meta property='og:type' content={pathname ? 'article' : 'website'} />
 
+			{/* TWITTER META TAGS */}
 			<meta name='twitter:card' content='summary_large_image' />
 			<meta name='twitter:title' content={seo.title} />
-			<meta name='twitter:url' content={seo.url} />
 			<meta name='twitter:description' content={seo.description} />
 			<meta name='twitter:image' content={seo.image} />
-			<meta name='twitter:creator' content={seo.twitterUsername} />
-
-			{/* NICE! no incoming scripts & overall script! */}
+			{seo.url && <meta name='twitter:url' content={seo.url} />}
+			{seo.twitterUsername && (
+				<meta name='twitter:site' content={seo.twitterUsername} />
+			)}
+			{seo.twitterUsername && (
+				<meta name='twitter:creator' content={seo.twitterUsername} />
+			)}
 
 			{/* ICONS */}
 			<link
