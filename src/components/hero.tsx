7e95865881f533a9abe785d5d57dc1b6173ba819
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-08-03 00:38:14 +0200

    herotext only for home

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useLocation } from '@reach/router';
+
 import { Link } from 'gatsby';
 
 import { StaticImage } from 'gatsby-plugin-image';
@@ -7,24 +9,31 @@ import { StaticImage } from 'gatsby-plugin-image';
 import * as heroStyles from '../styles/modules/hero.module.scss';
 
 const Hero: React.FC = () => {
+	const { pathname } = useLocation();
+
+	const isHome = pathname === '/';
+
 	return (
 		<section className={heroStyles.hero}>
-			<div className={heroStyles.slogan}>
-				<h1>
-					<span>
-						{' '}
-						Welkom bij <strong>Keep It Real</strong>, een programma dat jongeren
-					</span>{' '}
-					<span>
-						een ervaring biedt die verder gaat dan{' '}
-						<strong>traditioneel onderwijs</strong>
-					</span>
-				</h1>
-				<div className={heroStyles.buttons}>
-					<Link to='/workshops/'>Workshops</Link>
-					<Link to='/contact/'>Contact</Link>
+			{isHome && (
+				<div className={heroStyles.slogan}>
+					<h1>
+						<span>
+							{' '}
+							Welkom bij <strong>Keep It Real</strong>, een programma dat
+							jongeren
+						</span>{' '}
+						<span>
+							een ervaring biedt die verder gaat dan{' '}
+							<strong>traditioneel onderwijs</strong>
+						</span>
+					</h1>
+					<div className={heroStyles.buttons}>
+						<Link to='/workshops/'>Workshops</Link>
+						<Link to='/contact/'>Contact</Link>
+					</div>
 				</div>
-			</div>
+			)}
 
 			<div className={heroStyles.line} />
 
